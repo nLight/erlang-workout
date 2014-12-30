f0145385888208e1fc0a8113e0d6b8a73af0819c
Dmitriy Rozhkov <dmitriy.rozhkov@xing.com>
2014-12-30 11:13:26 +0100

    OTP version

--- tic-tac-toe/game.erl
+++ tic-tac-toe/game.erl
@@ -1,10 +1,44 @@
 -module(game).
--export([start/0]).
+-behaviour(gen_server).
+-export([init/1, handle_call/3, terminate/2]).
+-export([start_link/0, move/4, print_game/1]).
 
+start_link() -> gen_server:start_link(?MODULE, [], []).
 
-%% Entry point
-start() ->
-  spawn(fun() -> loop([z, z, z, z, z, z, z, z, z]) end).
+%% Make a move
+move(Pid, Player, X, Y) ->
+  gen_server:call(Pid, {Player, X, Y}).
+
+%% Prints game field
+print_game(Pid) -> gen_server:call(Pid, print).
+
+%% Server
+init([]) ->
+  {ok, {[z, z, z, z, z, z, z, z, z], nobody}}.
+
+terminate(_Status, _State) -> ok.
+
+handle_call({LastPlayer, _X, _Y}, _From, {Game, LastPlayer}) ->
+  {reply, "Wrong player!", {Game, LastPlayer}};
+
+handle_call({Player, X, Y}, _From, {Game, _LastPlayer}) when Player =:= o ; Player =:= x ->
+  case check_pos(Game, X, Y) of
+    z ->
+      NewGame = set_player(Game, Player, X, Y),
+      case check_winner(NewGame, Player) of
+        true  -> {stop, normal, "Winner!", {NewGame, Player}};
+        false -> {reply, "Next", {NewGame, Player}}
+      end;
+    _ ->
+      {reply, "Wrong move!", {Game, Player}}
+  end;
+
+handle_call(print, _From, {Game, LastPlayer}) ->
+  Reply = io_lib:format("~p~n~p~n~p~n~nLast Player: ~p~n",
+                        [lists:sublist(Game, 3),
+                         lists:sublist(Game, 4, 3),
+                         lists:sublist(Game, 7, 3), LastPlayer]),
+  {reply, Reply, {Game, LastPlayer}}.
 
 %% Sets a player to a position
 set_player(Game, Player, X, Y) ->
@@ -29,41 +63,4 @@ check_winner(Game, P) ->
     [P, _, _, _, P, _, _, _, P ] -> true;
     [_, _, P, _, P, _, P, _, _ ] -> true;
     _else -> false
-  end.
-
-%% Prints game field
-print_game(Game) ->
-  io:format("~p~n~p~n~p~n~n", [lists:sublist(Game, 3), lists:sublist(Game, 4, 3), lists:sublist(Game, 7, 3)]).
-
-%% Returns next player
-next_player(LastPlayer) ->
-  case LastPlayer of
-    0 -> x;
-    x -> 0
-  end.
-
-%% Main loop
-loop(Game) ->
-  io:format("Starting a new game.~n"),
-  loop(Game, 0).
-
-loop(Game, LastPlayer) ->
-  print_game(Game),
-  Player = next_player(LastPlayer),
-  receive
-    {Player, X, Y} ->
-      case check_pos(Game, X, Y) of
-        z ->
-          NewGame = set_player(Game, Player, X, Y),
-          case check_winner(NewGame, Player) of
-            true  -> io:format("{} is a winner!~n", Player);
-            false -> loop(NewGame, Player)
-          end;
-        _ ->
-          io:format("Wrong move!~n"),
-          loop(Game, Player)
-      end;
-    {LastPlayer, _X, _Y} ->
-      io:format("Wrong player!~n"),
-      loop(Game, LastPlayer)
   end.
